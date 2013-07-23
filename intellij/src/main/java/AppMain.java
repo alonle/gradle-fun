c7591ea8dc0ed618eca2d17a6e200e3a1cc22635
alonle <alon.levi@kenshoo.com>
2013-07-23 14:01:04 +0300

    first commit

---
+++ intellij/src/main/java/AppMain.java
@@ -0,0 +1,12 @@
+
+
+
+class AppMain {
+
+
+    public static void main(String[] args) throws Exception {
+
+    }
+
+
+}
---
+++ player/src/test/java/PlayerTest.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+
+public class PlayerTest {
+
+    @Test
+    public void testString() throws Exception {
+        Thread.sleep(10*1000);
+        assertEquals("mrhaki", "mr" + "haki");
+    }
+
+}
